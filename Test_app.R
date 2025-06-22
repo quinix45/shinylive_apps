bf71ea71ca32e760e52cefde0ae86f22a1302580
Fabio Setti <fabio.setti45@gmail.com>
2025-06-22 10:36:12 -0400

    Added p_value shiny app for normal distribution (no two tailed p-values, because I dont like choosing alpha levels)

---
+++ Test_app.R
@@ -0,0 +1,120 @@
+library(shiny)
+library(munsell)
+library(ggplot2)
+library(bslib)
+
+theme_set(theme_classic(base_size = 16, 
+                        base_family = 'serif'))
+
+ui <- bslib::page_fluid(
+  # Main plot area
+  
+  mainPanel(
+    
+    fluidRow(column(12, plotOutput("Sampling_Distribution")))),
+  
+  fluidRow(
+    column(2, numericInput("Mean", "\u03BC", value = 10, step = 1)),
+    column(2, numericInput("SD", "\u03C3", value = 3, min = 0.1, max = 10, step = 0.25)),
+    column(2, numericInput("N", "N", value = 25, min = 2, step = 1)),
+    column(2, numericInput("SM", "X\u0304", value = 9, step = .1)),
+    column(2,  selectInput("tail", "Tail",
+                           c("Lower" = "low",
+                             "Upper" = "up")))
+  ))
+
+
+
+server <- function(input, output, session) {
+  
+  output$Sampling_Distribution <- renderPlot({
+    # Get values from inputs
+    
+    
+    if(input$tail == "low"){
+    # calculate p-value
+    
+    p_val <- pnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))
+    
+    p_val <- ifelse(p_val < .001, "p < .001", paste("p =", round(p_val, 3)))
+    
+    # Create the plot
+    ggplot() +
+      xlim(input$Mean-3*(input$SD)/sqrt(input$N), input$Mean + 3*(input$SD)/sqrt(input$N))+
+      geom_function(fun = dnorm, args = list(mean = input$Mean,
+                                             sd = input$SD/sqrt(input$N)),
+                    color = "#1b305c") +
+      xlab("Sampling Distribution") +
+      geom_segment(aes(x = input$SM,
+                       xend = input$SM,
+                       y = 0,
+                       yend = dnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))),
+                   lty = 2) +
+      geom_ribbon(data = data.frame(x = seq(input$SM, input$SM - (abs(input$SM)*input$SD), length.out = 10000)), 
+                  aes(x = x, ymin = 0, ymax = dnorm(x, mean = input$Mean, sd = input$SD/sqrt(input$N))), 
+                  fill = "#1b305c", alpha = 0.5) +
+      
+      annotate("text", 
+              x = Inf,
+              y = Inf,
+              label = p_val, hjust=1,vjust = 1) +
+      scale_y_continuous(expand = c(0,0)) + 
+      theme( axis.text.y=element_blank(),
+             axis.ticks.y=element_blank(),
+             axis.line.y = element_blank(),
+             axis.title.y = element_blank())
+    
+    }else{
+      
+      
+      p_val <- 1 - pnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))
+      
+      p_val <- ifelse(p_val < .001, "p < .001", paste("p =", round(p_val, 3)))
+      
+      # Create the plot
+      ggplot() +
+        xlim(input$Mean-3*(input$SD)/sqrt(input$N), input$Mean + 3*(input$SD)/sqrt(input$N))+
+        geom_function(fun = dnorm, args = list(mean = input$Mean,
+                                               sd = input$SD/sqrt(input$N)),
+                      color = "#1b305c") +
+        xlab("Sampling Distribution") +
+        geom_segment(aes(x = input$SM,
+                         xend = input$SM,
+                         y = 0,
+                         yend = dnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))),
+                     lty = 2) +
+        geom_ribbon(data = data.frame(x = seq(input$SM, input$SM + (abs(input$SM)*input$SD), length.out = 10000)), 
+                    aes(x = x, ymin = 0, ymax = dnorm(x, mean = input$Mean, sd = input$SD/sqrt(input$N))), 
+                    fill = "#1b305c", alpha = 0.5) +
+        
+        annotate("text", 
+                 x = Inf,
+                 y = Inf,
+                 label = p_val, hjust=1,vjust = 1) +
+        scale_y_continuous(expand = c(0,0)) + 
+        theme( axis.text.y=element_blank(),
+               axis.ticks.y=element_blank(),
+               axis.line.y = element_blank(),
+               axis.title.y = element_blank())
+      
+      
+    }
+    
+    
+    
+  }, res = 100)
+}
+
+
+# Run the Shiny app
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
--- app.R
+++ app.R
@@ -6,100 +6,109 @@ library(bslib)
 theme_set(theme_classic(base_size = 16, 
                         base_family = 'serif'))
 
-
-
 ui <- bslib::page_fluid(
   # Main plot area
   
   mainPanel(
     
-    fluidRow(column(6, plotOutput("Population")),
-             column(6, plotOutput("Sample")))),
+    fluidRow(column(12, plotOutput("Sampling_Distribution")))),
   
   fluidRow(
-    column(2, numericInput("Mean", "\u03BC", value = 5, min = -100, max = 100, step = 1)),
+    column(2, numericInput("Mean", "\u03BC", value = 10, step = 1)),
     column(2, numericInput("SD", "\u03C3", value = 3, min = 0.1, max = 10, step = 0.25)),
-    column(2, numericInput("SS", "Sample Size", value = 100, min = 2, max = 20000, step = 10)),
-    column(2, numericInput("NS", "Experiments", value = 10, min = 1, max = 20000, step = 10))
-    , column(2, actionButton("start", "Run Experiments"))
-  )
-)
+    column(2, numericInput("N", "N", value = 25, min = 2, step = 1)),
+    column(2, numericInput("SM", "X\u0304", value = 9, step = .1)),
+    column(2,  selectInput("tail", "Tail",
+                           c("Lower" = "low",
+                             "Upper" = "up")))
+  ))
 
 
 
 server <- function(input, output, session) {
   
-  output$Population <- renderPlot({
+  output$Sampling_Distribution <- renderPlot({
     # Get values from inputs
     
     
+    if(input$tail == "low"){
+      # calculate p-value
+      
+      p_val <- pnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))
+      
+      p_val <- ifelse(p_val < .001, "p < .001", paste("p =", round(p_val, 3)))
+      
+      # Create the plot
+      ggplot() +
+        xlim(input$Mean-3*(input$SD)/sqrt(input$N), input$Mean + 3*(input$SD)/sqrt(input$N))+
+        geom_function(fun = dnorm, args = list(mean = input$Mean,
+                                               sd = input$SD/sqrt(input$N)),
+                      color = "#1b305c") +
+        xlab("Sampling Distribution") +
+        geom_segment(aes(x = input$SM,
+                         xend = input$SM,
+                         y = 0,
+                         yend = dnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))),
+                     lty = 2) +
+        geom_ribbon(data = data.frame(x = seq(input$SM, input$SM - (abs(input$SM)*input$SD), length.out = 10000)), 
+                    aes(x = x, ymin = 0, ymax = dnorm(x, mean = input$Mean, sd = input$SD/sqrt(input$N))), 
+                    fill = "#1b305c", alpha = 0.5) +
+        
+        annotate("text", 
+                 x = Inf,
+                 y = Inf,
+                 label = p_val, hjust=1,vjust = 1) +
+        scale_y_continuous(expand = c(0,0)) + 
+        theme( axis.text.y=element_blank(),
+               axis.ticks.y=element_blank(),
+               axis.line.y = element_blank(),
+               axis.title.y = element_blank())
+      
+    }else{
+      
+      
+      p_val <- 1 - pnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))
+      
+      p_val <- ifelse(p_val < .001, "p < .001", paste("p =", round(p_val, 3)))
+      
+      # Create the plot
+      ggplot() +
+        xlim(input$Mean-3*(input$SD)/sqrt(input$N), input$Mean + 3*(input$SD)/sqrt(input$N))+
+        geom_function(fun = dnorm, args = list(mean = input$Mean,
+                                               sd = input$SD/sqrt(input$N)),
+                      color = "#1b305c") +
+        xlab("Sampling Distribution") +
+        geom_segment(aes(x = input$SM,
+                         xend = input$SM,
+                         y = 0,
+                         yend = dnorm(input$SM, mean = input$Mean, sd = input$SD/sqrt(input$N))),
+                     lty = 2) +
+        geom_ribbon(data = data.frame(x = seq(input$SM, input$SM + (abs(input$SM)*input$SD), length.out = 10000)), 
+                    aes(x = x, ymin = 0, ymax = dnorm(x, mean = input$Mean, sd = input$SD/sqrt(input$N))), 
+                    fill = "#1b305c", alpha = 0.5) +
+        
+        annotate("text", 
+                 x = Inf,
+                 y = Inf,
+                 label = p_val, hjust=1,vjust = 1) +
+        scale_y_continuous(expand = c(0,0)) + 
+        theme( axis.text.y=element_blank(),
+               axis.ticks.y=element_blank(),
+               axis.line.y = element_blank(),
+               axis.title.y = element_blank())
+      
+      
+    }
     
-    # Create the plot
-    ggplot() +
-      xlim(input$Mean-3*input$SD, input$Mean + 3*input$SD)+
-      geom_function(fun = dnorm, args = list(mean = input$Mean,
-                                             sd = input$SD),
-                    color = "#1b305c") +
-      xlab("Population Distribution") +
-      geom_segment(aes(x = input$Mean,
-                       xend = input$Mean,
-                       y = 0,
-                       yend = dnorm(input$Mean, mean = input$Mean, sd = input$SD)),
-                   lty = 2) +
-      scale_y_continuous(expand = c(0,0)) + 
-      theme( axis.text.y=element_blank(),
-             axis.ticks.y=element_blank(),
-             axis.line.y = element_blank(),
-             axis.title.y = element_blank())
-    
-    
-    
-    
-  }, res = 100)
-  
-  
-  
-  
-  
-  
-  sample <- eventReactive(input$start, {data.frame(colMeans(replicate(input$NS, 
-                                                                      rnorm(n = input$SS, 
-                                                                            mean = input$Mean, 
-                                                                            sd = input$SD))))})
-  
-  
-  
-  output$Sample <- renderPlot({
-    # Get values from inputs
-    
-    rand_sample <- sample()
-    
-    
-    colnames(rand_sample) <- "X"
-    
-    ggplot(rand_sample, aes(x = X)) +
-      xlab(paste("Means for", nrow(rand_sample), "Experiments")) +
-      scale_y_continuous(expand = c(0,0)) +
-      geom_histogram(color = "black",
-                     linewidth = .8,
-                     fill = "#1b305c",
-                     bins = 30) +
-      annotate("text", 
-               x = Inf,
-               y = Inf,
-               label = paste0("Mean =", round(mean(rand_sample$X), 2),"\n",
-                              "SD (aka SE) =", round(sd(rand_sample$X), 2)), hjust=1,vjust = 1) + 
-      theme( axis.text.y=element_blank(),
-             axis.ticks.y=element_blank(),
-             axis.line.y = element_blank(),
-             axis.title.y = element_blank())
     
     
   }, res = 100)
 }
 
+
 # Run the Shiny app
 shinyApp(ui = ui, server = server)
+
 
 # shinylive::export(appdir = ".",
 #                   destdir = "docs/")
